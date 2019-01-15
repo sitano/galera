27d46b54683abd27e73bd0a498703c2d4f5e821d
Alexey Yurchenko <alexey.yurchenko@galeracluster.com>
2019-01-15 02:07:27 +0200

    Refs codership/galera-bugs#165 Mock encryption callback for unit tests
    Added mock encryption callback for subsequent use in unit tests and
    unit tests for callback itself.

---
+++ gcache/src/gcache_test_encryption.cpp
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2019 Codership Oy <info@codership.com>
+ */
+
+#include "gcache_test_encryption.hpp"
+
+//remove #include <gu_macros.hpp>
+#include <gu_digest.hpp>
+
+#define NAMESPACE_GCACHE_TEST_BEGIN namespace gcache { namespace test {
+#define NAMESPACE_GCACHE_TEST_END   } /* test */ } /* gcache */
+
+NAMESPACE_GCACHE_TEST_BEGIN
+
+class Encrypt
+{
+    static int const BLOCKSIZE = 16;
+    static_assert(BLOCKSIZE < sizeof(wsrep_enc_iv_t),
+                  "blocksize bigger than IV");
+    static_assert(0 == BLOCKSIZE % sizeof(size_t),
+                  "blocksize is not multiple of size_t size");
+
+    static int const BLOCKLEN = BLOCKSIZE / sizeof(size_t);
+
+    union block {
+        char   c[BLOCKSIZE];
+        size_t s[BLOCKLEN];
+    };
+    static_assert(sizeof(block::c) == sizeof(block::s), "size differs");
+    static_assert(sizeof(block) == BLOCKSIZE, "something's wrong");
+
+    static inline
+    void XOR_block(const block& l, const block& r, block& out)
+    {
+        for (int i(0); i < BLOCKLEN; ++i) { out.s[i] = l.s[i] ^ r.s[i]; }
+    }
+
+    static inline
+    void ENC_block(const block& k, const block& in, block& out)
+    {
+        XOR_block(k, in, out);
+    }
+
+public:
+
+    static int
+    CFB(
+        void*                 const app_ctx,
+        wsrep_enc_ctx_t*      const enc_ctx,
+        const wsrep_buf_t*    const input,
+        void*                 const output,
+        wsrep_enc_direction_t const direction,
+        bool                  const fin
+        )
+    {
+        typedef unsigned int uint;
+        struct op_ctx { block iv; block key; } ctx;
+
+        if (NULL != enc_ctx->ctx)
+        {
+            ctx = *static_cast<op_ctx*>(enc_ctx->ctx);
+        }
+        else
+        {
+            static_assert(sizeof(ctx.iv) <= sizeof(*enc_ctx->iv),
+                          "IV block too big");
+            ::memcpy(&ctx.iv, enc_ctx->iv, sizeof(ctx.iv));
+            gu::MMH3::digest(enc_ctx->key->ptr, enc_ctx->key->len, ctx.key);
+            // @todo: what if block size > 16?
+        }
+
+        uint rem(input->len);
+        const char* in(static_cast<const char*>(input->ptr));
+        char* out(static_cast<char*>(output));
+
+        while (rem > sizeof(block) || (fin && rem > 0))
+        {
+            uint const cpy(rem > sizeof(block) ? sizeof(block) : rem);
+            assert(cpy == sizeof(block) || fin);
+
+            block b;
+
+            if (direction == WSREP_ENC)
+            {
+                ENC_block(ctx.key, ctx.iv, ctx.iv);
+                ::memcpy(&b, in, cpy);
+                XOR_block(b, ctx.iv, ctx.iv);
+                ::memcpy(out, &ctx.iv, cpy);
+            }
+            else
+            {
+                ENC_block(ctx.key, ctx.iv, b);
+                ::memcpy(&ctx.iv, in, cpy);
+                XOR_block(ctx.iv, b, b);
+                ::memcpy(out, &b, cpy);
+            }
+
+            assert(rem >= cpy);
+            rem -= cpy;
+            if (rem > 0)
+            {
+                in  += sizeof(block);
+                out += sizeof(block);
+            }
+        }
+
+        if (!fin)
+        {
+            if (NULL == enc_ctx->ctx)
+                enc_ctx->ctx = ::operator new(sizeof(op_ctx));
+
+            *static_cast<op_ctx*>(enc_ctx->ctx) = ctx;
+        }
+        else
+        {
+            ::operator delete(enc_ctx->ctx);
+            enc_ctx->ctx = NULL;
+        }
+
+        assert(input->len >= rem);
+        return (input->len - rem);
+    }
+}; /* class Encrypt */
+
+NAMESPACE_GCACHE_TEST_END
+
+extern "C"
+int gcache_test_encrypt_cb(
+    void*                 const app_ctx,
+    wsrep_enc_ctx_t*      const enc_ctx,
+    const wsrep_buf_t*    const input,
+    void*                 const output,
+    wsrep_enc_direction_t const direction,
+    bool                  const fin
+    )
+{
+    return gcache::test::Encrypt::CFB
+        (app_ctx, enc_ctx, input, output, direction, fin);
+}
---
+++ gcache/tests/gcache_enc_test.cpp
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2019 Codership Oy <info@codership.com>
+ *
+ * $Id$
+ */
+
+#include "gcache_test_encryption.hpp"
+#include "gcache_enc_test.hpp"
+
+#include <cassert>
+
+static wsrep_buf_t const key1 = { "1", 1 };
+static wsrep_buf_t const key2 = { "2", 1 };
+
+static wsrep_enc_iv_t const iv1 = { 1, };
+static wsrep_enc_iv_t const iv2 = { 2, };
+
+static wsrep_enc_ctx_t ctx11 = { &key1, &iv1, NULL };
+static wsrep_enc_ctx_t ctx12 = { &key1, &iv2, NULL };
+static wsrep_enc_ctx_t ctx21 = { &key2, &iv1, NULL };
+
+static char source[] = "Nothing is covered up that will not be revealed, "
+                       "or hidden that will not be known.";
+static_assert(sizeof(source) == 83, "source length is not a prime number");
+/* prime number means it is not multiple of any block size */
+
+/* tests empty message encryption */
+static void
+null_test(wsrep_encrypt_cb_t const cb, void* app_ctx, size_t const blocksize)
+{
+    wsrep_buf_t in = { NULL, 0 };
+    char out(0);
+
+    int ret(cb(app_ctx, &ctx11, &in, &out, WSREP_ENC, true));
+    fail_if(ret != 0);
+    fail_if(out != 0);
+}
+
+START_TEST(null_test)
+{
+    null_test(gcache_test_encrypt_cb, NULL, 16);
+}
+END_TEST
+
+/* tests atomic message encryption */
+static void
+fin_test(wsrep_encrypt_cb_t const cb, void* app_ctx, size_t const blocksize)
+{
+    assert(sizeof(source) % blocksize);
+    wsrep_buf_t orig = { source, sizeof(source) };
+    char cipher11[sizeof(source)];
+    char cipher12[sizeof(source)];
+    char cipher21[sizeof(source)];
+
+    int ret(cb(app_ctx, &ctx11, &orig, cipher11, WSREP_ENC, true));
+    fail_if(ret != sizeof(source));
+    fail_if(!::memcmp(orig.ptr, cipher11, ret));
+
+    ret = cb(app_ctx, &ctx12, &orig, cipher12, WSREP_ENC, true);
+    fail_if(ret != sizeof(source));
+    fail_if(!::memcmp(orig.ptr, cipher12, ret));
+    fail_if(!::memcmp(cipher11, cipher12, ret));
+
+    ret = cb(app_ctx, &ctx21, &orig, cipher21, WSREP_ENC, true);
+    fail_if(ret != sizeof(source));
+    fail_if(!::memcmp(orig.ptr, cipher21, ret));
+    fail_if(!::memcmp(cipher11, cipher21, ret));
+
+    wsrep_buf_t res = { cipher11, sizeof(cipher11) };
+    char plain[sizeof(cipher11)];
+
+    ret = cb(app_ctx, &ctx11, &res, plain, WSREP_DEC, true);
+    fail_if(ret != sizeof(cipher11));
+    fail_if(::memcmp(orig.ptr, plain, ret), "Expected:\n%s\nGot:\n%s",
+            static_cast<const char*>(orig.ptr), plain);
+
+    ret = cb(app_ctx, &ctx12, &res, plain, WSREP_DEC, true);
+    fail_if(ret != sizeof(cipher11));
+    fail_if(!::memcmp(orig.ptr, plain, ret));
+
+    ret = cb(app_ctx, &ctx21, &res, plain, WSREP_DEC, true);
+    fail_if(ret != sizeof(cipher11));
+    fail_if(!::memcmp(orig.ptr, plain, ret));
+}
+
+START_TEST(fin_test)
+{
+    fin_test(gcache_test_encrypt_cb, NULL, 16);
+}
+END_TEST
+
+/* test stream encryption */
+static void
+stream_test(wsrep_encrypt_cb_t const cb, void* app_ctx, size_t const blocksize)
+{
+    assert(sizeof(source) % blocksize);
+    wsrep_enc_ctx_t const comp_ctx = ctx11;
+
+    char cipher1sweep[sizeof(source)]; /* etalon encryption result */
+    {
+        wsrep_buf_t in = { source, sizeof(source) };
+        int ret(cb(app_ctx, &ctx11, &in, cipher1sweep, WSREP_ENC, true));
+        fail_if(ret != sizeof(source));
+        fail_if(::memcmp(&comp_ctx, &ctx11, sizeof(ctx11)));
+    }
+
+    char cipher[sizeof(source)];
+    int ret(-1);
+    const char* src(source);
+    char* out(cipher);
+    unsigned int left(sizeof(source));
+    while(left > 0)
+    {
+        wsrep_buf_t in = { src, (ret != 0 ? left/2 : left) };
+        ret = cb(app_ctx, &ctx11, &in, out, WSREP_ENC, 0 == ret);
+
+        fail_if(ret < 0);
+        fail_if(ret > int(in.len));
+        src += ret;
+        out += ret;
+        left-= ret;
+    }
+
+    fail_if(left != 0);
+    fail_if(!::memcmp(source, cipher, sizeof(source)));
+    fail_if(::memcmp(cipher1sweep, cipher, sizeof(cipher)));
+    fail_if(::memcmp(&comp_ctx, &ctx11, sizeof(ctx11)));
+
+    char plain [sizeof(source)];
+    ret = -1;
+    src = cipher;
+    out = plain;
+    left = sizeof(cipher);
+    while(left > 0)
+    {
+        wsrep_buf_t in = { src, (ret != 0 ? left/2 : left) };
+        ret = cb(app_ctx, &ctx11, &in, out, WSREP_DEC, 0 == ret);
+
+        fail_if(ret < 0);
+        fail_if(ret > int(in.len));
+        src += ret;
+        out += ret;
+        left-= ret;
+    }
+
+    fail_if(left != 0);
+    fail_if(::memcmp(source, plain, sizeof(source)), "Expected:\n%s\nGot:\n%s",
+            source, plain);
+    fail_if(::memcmp(&comp_ctx, &ctx11, sizeof(ctx11)));
+}
+
+START_TEST(stream_test)
+{
+    stream_test(gcache_test_encrypt_cb, NULL, 16);
+}
+END_TEST
+
+Suite* gcache_enc_suite()
+{
+    Suite* s = suite_create("gcache::test::Encryption");
+    TCase* tc;
+
+    tc = tcase_create("test");
+    tcase_add_test(tc, null_test);
+    tcase_add_test(tc, fin_test);
+    tcase_add_test(tc, stream_test);
+    suite_add_tcase(s, tc);
+
+    return s;
+}
